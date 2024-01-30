7a169336cda81da866802faa06ac0d2c4a8c3da1
Benjamin Žagar <benjamin.zagar@barrage.net>
2024-01-30 11:30:18 +0100

    Added crud operations according to task 2

--- src/main/java/com/setronica/eventing/web/EventController.java
+++ src/main/java/com/setronica/eventing/web/EventController.java
@@ -2,10 +2,11 @@ package com.setronica.eventing.web;
 
 import com.setronica.eventing.app.EventService;
 import com.setronica.eventing.persistence.Event;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import com.setronica.eventing.persistence.EventUpdate;
+import jakarta.validation.Valid;
+import org.springframework.dao.DataAccessException;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -20,14 +21,39 @@ public class EventController {
     }
 
     @GetMapping
-    public List<Event> findAll() {
-        return eventService.getAll();
+    public ResponseEntity<List<Event>> all() {
+        try {
+            List<Event> allEvents = eventService.getAll();
+            if (allEvents.isEmpty()) {
+                return ResponseEntity.notFound().build();
+            }
+            return ResponseEntity.ok(allEvents);
+        } catch (DataAccessException e) {
+            return ResponseEntity.internalServerError().build();
+        }
     }
 
-    @GetMapping("search")
-    public List<Event> searchEvents(
-            @RequestParam String q
+    @GetMapping("{id}")
+    public Event show(
+            @PathVariable int id
     ) {
-        return eventService.getByTitle(q);
+        return eventService.findById(id);
+    }
+
+    @PostMapping
+    public Event create(@Valid @RequestBody Event event) {
+           return eventService.save(event);
+    }
+
+    @PutMapping("{id}")
+    public Event update(@PathVariable int id, @RequestBody EventUpdate updatedEvent) {
+            Event existingEvent = eventService.findById(id);
+            return eventService.update(updatedEvent, existingEvent);
+    }
+
+    @DeleteMapping("{id}")
+    public void delete(@PathVariable int id) {
+        Event existingEvent = eventService.findById(id);
+        eventService.delete(id);
     }
 }
